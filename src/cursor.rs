db4e50633b2f8733d637abb907226194e78980c2
otis <electricbuck@gmail.com>
2024-07-12 18:41:42 -0400

    matching wip

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,7 +1,8 @@
 //cursor
 
 use crate::tiles::{
-    position_and_z_to_transform, position_to_transform, Board, Position, BOARD_SIZE,
+    position_and_z_to_transform, position_to_transform, Board, Color, Position, Shape, Tile,
+    BOARD_SIZE,
 };
 use bevy::prelude::*;
 use leafwing_input_manager::prelude::*;
@@ -11,7 +12,7 @@ pub struct CursorPlugin;
 impl Plugin for CursorPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(InputManagerPlugin::<CursorAction>::default());
-        app.add_systems(Update, (move_cursor, mark).chain());
+        app.add_systems(Update, (move_cursor, mark, check_match).chain());
     }
 }
 
@@ -82,8 +83,8 @@ pub fn mark(
     for (position, action_state) in query.iter() {
         if action_state.just_pressed(&CursorAction::Select) {
             if let Some(Some(entity)) = board.tiles.get(position.x, position.y) {
-                commands.entity(*entity).despawn();
-                // commands.entity(*entity).insert(Mark);
+                // commands.entity(*entity).despawn();
+                commands.entity(*entity).insert(Mark);
                 // println!("marked tile at {:?}", position);
                 // println!("despawned tile at {:?}", position);
                 // } else {
@@ -92,3 +93,68 @@ pub fn mark(
         }
     }
 }
+
+fn check_match(
+    query: Query<(&Color, &Shape), (With<Tile>, With<Mark>)>,
+    entities: Query<Entity, (With<Mark>)>,
+    mut commands: Commands,
+) {
+    let (colors, shapes): (Vec<&Color>, Vec<&Shape>) = query.iter().unzip();
+    //TODO:
+    if colors.len() < 3 {
+        return;
+    }
+    let mut unique_colors = colors.clone();
+    unique_colors.sort();
+    unique_colors.dedup();
+    let color_match = colors.len() == unique_colors.len() || unique_colors.len() == 1;
+    let mut shape_match = false;
+    let mut unique_shapes = shapes.clone();
+    unique_shapes.sort();
+    unique_shapes.dedup();
+    let shape_match = shapes.len() == unique_shapes.len() || unique_shapes.len() == 1;
+
+    if shape_match && color_match {
+        for entity in entities.iter() {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+// fn check_match(
+//     query: Query<(&Color, &Shape), With<Mark>>,
+//     entities: Query<(Entity, &TilePos), (With<Mark>, With<Color>, With<Shape>)>,
+//     mut match_event: EventWriter<RemoveEvent>,
+//     mut commands: Commands,
+// ) {
+//     let (mut colors, mut shapes): (Vec<&Color>, Vec<&Shape>) = query.iter().unzip();
+//     let mut color_match = false;
+//     colors.sort_unstable();
+//     // println!("{:?}", colors);
+//     let mut unique_colors = colors.clone();
+//     unique_colors.dedup();
+//     // println!("{:?}", unique_colors);
+//     if colors.len() == unique_colors.len() || unique_colors.len() == 1 {
+//         color_match = true;
+//     } else {
+//         // println!("no match")
+//     }
+//     let mut shape_match = false;
+//     shapes.sort_unstable();
+//     // println!("{:?}", shapes);
+//     let mut unique_shapes = shapes.clone();
+//     unique_shapes.dedup();
+//     // println!("{:?}", unique_shapes);
+//     if shapes.len() == unique_shapes.len() || unique_shapes.len() == 1 {
+//         shape_match = true;
+//     } else {
+//         // println!("no match")
+//     }
+//     if shape_match && color_match {
+//         println!("Match!!");
+//         for (entity, pos) in entities.iter() {
+//             match_event.send(RemoveEvent(entity, *pos))
+//         }
+//     }
+//     commands.insert_resource(NextState(GameState::Match));
+// }
